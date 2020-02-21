627d8082cf530f3d5086bbff136a45e4f77628ff
pmcu-scl <46488606+pmcu-scl@users.noreply.github.com>
2020-02-21 15:33:12 +0000

    Add files via upload

---
+++ csvRead-1.py
@@ -0,0 +1,15 @@
+﻿import csv
+
+with open('sonra-1.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'{", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} - {row[1]} - {row[2]}.')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
+
+cat=input("Imeacht?")
---
+++ parse-csv.py
@@ -0,0 +1,7 @@
+﻿import csv
+
+with open('sonra-1.csv','r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)
+    for line in csv_reader:
+        print(line)
